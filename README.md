af936845e933515dff6294eef3f76e7ceb01dd21
Paul Heasley <paul@phdesign.com.au>
2019-04-01 21:38:08 -0400

    Some refactoring

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # mtg-deck-finder
 
+Attempts to find Magic The Gathering decks that can be built from an inventory.
+
+No depenendencies.
+Requires Python 3.6+
+
+## Usage 
+
 ```
-python -m mtg_deck_finder -i deck.dec inventory.txt
+usage: mtg_deck_finder [-h] [-o {table,json,percent}] -i INVENTORY [deck]
+
+attempts to find magic the gathering decks that can be built from an inventory
+
+positional arguments:
+  deck                  deck to compare against inventory
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o {table,json,percent}, --output-format {table,json,percent}
+                        output format, "table", "json" or "percent"
+  -i INVENTORY, --inventory INVENTORY
+                        inventory file of mtg cards available
+```
+
+e.g.
+
+```
+python -m mtg_deck_finder -i deck.dec -o json inventory.txt
+```
+
+## Examples
+
+Process a folder of decks
+
+```
+find decks/forge-net-decks-2018-10-04/ -iname *.dck -exec python3 -m mtg_deck_finder -i decks/inventory.txt -o percent "{}" \;
 ```
--- mtg_deck_finder/__main__.py
+++ mtg_deck_finder/__main__.py
@@ -1,35 +1,45 @@
+import os
+import json
 from functools import reduce
-import itertools
 import collections
 from .deck import DeckReader
 from .config import Config
 
-ComparisonEntry = collections.namedtuple('ComparisonEntry', ['name', 'deck_count', 'inventory_count'])
+Comparison = collections.namedtuple('Comparison', ['deck_name', 'cards', 'total', 'similarity'])
 
-def compare(deck, inventory):
-    cards = [ComparisonEntry(
-        name=e.name,
-        deck_count=e.count,
-        inventory_count=next((i.count for i in inventory if i.name == e.name), 0)
-    ) for e in deck]
-    return cards
+def compare(deck_name, deck, inventory):
+    cards = [{**c, 'needed': max(c['deck_count'] - c['inventory_count'], 0)} for c in ({
+        'name': e.name,
+        'deck_count': e.count,
+        'inventory_count': next((i.count for i in inventory if i.name == e.name), 0)
+    } for e in deck)]
+    totals = reduce(lambda acc, c: {
+        'name': 'Total',
+        'deck_count': acc['deck_count'] + c['deck_count'],
+        'inventory_count': acc['inventory_count'] + c['inventory_count'],
+        'needed': acc['needed'] + c['needed']
+    }, cards)
+    matching_count = sum(c['inventory_count'] for c in cards)
+    deck_count = sum(c['deck_count'] for c in cards)
+    similarity = round((matching_count / deck_count) * 100, 2)
+    return Comparison(
+        deck_name=deck_name,
+        cards=cards,
+        total=totals,
+        similarity=similarity
+    )
 
-def calc_similarity(cards):
-    matching_count = sum(c.inventory_count for c in cards)
-    deck_count = sum(c.deck_count for c in cards)
-    return round((matching_count / deck_count) * 100, 2)
+def print_percent(comparison):
+    print(comparison.similarity)
 
-def print_percent(cards):
-    print(calc_similarity(cards))
-
-def print_table(cards):
-    """
----------------------------------------------------
+def print_table(comparison):
+    """ Prints a table based view of the results, e.g.
+--------------------------------------------------
 Card         Deck Count   Inventory Count   Needed
----------------------------------------------------
+--------------------------------------------------
 Arena        4            2                 2
 Werebear     1            0                 1
----------------------------------------------------
+--------------------------------------------------
 Total        60           2                 3
 Similarity: 10%
     """
@@ -39,28 +49,25 @@ Similarity: 10%
         'inventory_count': 'Inventory Count',
         'needed': 'Needed'
     }
-    rows = [{**c._asdict(), 'needed': max(c.deck_count - c.inventory_count, 0)} for c in cards]
-    totals = reduce(lambda acc, c: {
-        'name': 'Total',
-        'deck_count': acc['deck_count'] + c['deck_count'],
-        'inventory_count': acc['inventory_count'] + c['inventory_count'],
-        'needed': acc['needed'] + c['needed']
-    }, rows)
-    col_lengths = ((len(str(v)) for v in card.values()) for card in [header, *rows])
+    col_lengths = ((len(str(v)) for v in card.values()) for card in [header, *comparison.cards, comparison.total])
     widths = list(map(max, zip(*col_lengths)))
     template = "{{name:{0}}}  {{deck_count:{1}}}  {{inventory_count:{2}}}  {{needed:{3}}}" \
         .format(*widths)
     line_length = sum(widths) + 6
     separator = '-' * line_length
 
+    print(comparison.deck_name)
     print(separator)
     print(template.format(**header))
     print(separator)
-    for row in rows:
-        print(template.format(**row))
+    for card in comparison.cards:
+        print(template.format(**card))
     print(separator)
-    print(template.format(**totals))
-    print("Similarity: {0}%".format(calc_similarity(cards)))
+    print(template.format(**comparison.total))
+    print("Similarity: {0}%".format(comparison.similarity))
+
+def print_json(comparison):
+    print(json.dumps(comparison._asdict()))
 
 def main():
     config = Config()
@@ -72,12 +79,15 @@ def main():
 
     deck = DeckReader(deck_str).read()
     inventory = DeckReader(inventory_str).read()
-    cards = compare(deck, inventory)
+    deck_name = os.path.basename(config.deck.name)
+    comparison = compare(deck_name, deck, inventory)
 
     if config.output_format == config.FORMAT_TABLE:
-        print_table(cards)
-    elif config.output_format == config.FORMAT_PERCENT:
-        print_percent(cards)
+        print_table(comparison)
+    elif config.output_format == config.FORMAT_JSON:
+        print_json(comparison)
+    else:
+        print_percent(comparison)
 
 if __name__ == '__main__':
     main()

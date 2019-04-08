b3ab7dae387d4361e8a3e1e2f7e8a7f57dd900b2
Paul Heasley <paul@phdesign.com.au>
2019-04-07 22:38:12 -0400

    Adding in deck_spider

---
+++ Makefile
@@ -0,0 +1,33 @@
+VENV_NAME ?= .venv
+PYTHON ?= python
+
+get_python_version = $(word 2,$(subst ., ,$(shell $(1) --version 2>&1)))
+ifneq ($(call get_python_version,$(PYTHON)), 3)
+    PYTHON = python3
+endif
+ifneq ($(call get_python_version,$(PYTHON)), 3)
+    $(error "No supported python found! Requires python v3.6+")
+endif
+
+ifdef OS
+    VENV_ACTIVATE ?= $(VENV_NAME)/Scripts/activate
+else
+    VENV_ACTIVATE ?= $(VENV_NAME)/bin/activate
+endif
+
+init:
+	test -d $(VENV_NAME) || $(PYTHON) -m venv $(VENV_NAME)
+	source $(VENV_ACTIVATE); \
+	pip install -r deck_spider/requirements.txt;
+
+spider:
+	@test -d $(VENV_NAME) && source $(VENV_ACTIVATE); \
+	scrapy runspider mtgtop8.py
+
+clean:
+	rm -rf .venv
+	rm -rf .pytest_cache
+	find . -iname "*.pyc" -delete
+	find . -type d -name "__pycache__" -delete
+
+.PHONY: init spider clean
---
+++ deck_spider/mtgtop8.py
@@ -0,0 +1,40 @@
+import os
+import re
+from urllib.parse import urlparse, parse_qs
+import scrapy
+
+class MtgTop8Spider(scrapy.Spider):
+    name = 'mtgtop8'
+
+    custom_settings = {
+        'EXTENSIONS': {
+            'scrapy.extensions.closespider.CloseSpider': 500
+        },
+        'CLOSESPIDER_ITEMCOUNT': 5,
+        'CONCURRENT_REQUESTS': 1
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(url='http://www.mtgtop8.com/search', callback=self.parse_search)
+
+    def parse_search(self, response):
+        for deck in response.css('.S11 a'):
+            yield response.follow(deck, callback=self.parse_card)
+        next_page_num = response \
+            .css('[name=compare_decks] table table tr:first-of-type td:last-of-type .Nav_PN::attr(onclick)') \
+            .re(r"PageSubmit\((\d+)\)")
+        if next_page_num:
+            data = {'current_page': next_page_num[0]}
+            yield scrapy.FormRequest(response.url, formdata=data)
+
+    def parse_card(self, response):
+        download = response.css('.Nav_link a')[0]
+        yield response.follow(download, callback=self.parse_download)
+
+    def parse_download(self, response):
+        filename = parse_qs(urlparse(response.url).query).get("f")[0] + '.txt'
+        filepath = os.path.join('download', filename)
+        if not os.path.exists(os.path.dirname(filepath)):
+            os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        with open(filepath, 'wb') as f:
+            f.write(response.body)
